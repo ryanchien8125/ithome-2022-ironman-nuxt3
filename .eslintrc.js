01f0d806451ddb60d724ff56ef00cdcc89b8f6fa
Ryan <ryanchien8125@gmail.com>
2022-09-20 19:25:33 +0800

    配置 Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,15 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:vue/vue3-recommended'],
+  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:vue/vue3-recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 13,
     sourceType: 'module'
   },
-  plugins: [],
-  rules: {},
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error'
+  },
   overrides: [
     {
       files: [
---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  printWidth: 100,          // 每行文字數量達 100 字元就換到新的一行
+  semi: false,              // 每個語句的結尾不需要分號
+  singleQuote: true,        // 字串使用單引號，而不是雙引號
+  trailingComma: 'none'     // 如 Object、Array 內的元素不需要尾隨逗號
+}
