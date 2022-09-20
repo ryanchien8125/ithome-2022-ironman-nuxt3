1f74a8c4931422998ee1f308bbb1a5b05f34391a
Ryan <ryanchien8125@gmail.com>
2022-09-20 19:28:46 +0800

    依照官方指引步驟導入 Tailwind CSS

--- app.vue
+++ app.vue
@@ -1,11 +1,8 @@
 <template>
-  <div>
-    <h1>{{ title }} - {{ name }}</h1>
+  <div class="bg-white py-24">
+    <div class="flex flex-col items-center">
+      <h1 class="text-6xl font-semibold text-blue-600">2022 iThome</h1>
+      <p class="mt-4 text-9xl font-bold text-gray-900">鐵人賽</p>
+    </div>
   </div>
 </template>
-
-<script lang="ts" setup>
-const year: number = 2022
-const title: string = `${year} iThome 鐵人賽`
-const name: string = 'Ryan'
-</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          'postcss-import': {},
+          'tailwindcss/nesting': {},
+          tailwindcss: {},
+          autoprefixer: {}
+        }
+      }
+    }
+  },
+  css: ['@/assets/css/tailwind.css'],
   typescript: {
     typeCheck: true
   }
