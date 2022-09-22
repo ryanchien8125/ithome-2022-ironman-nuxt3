8156c28019004738dd59e5b0eecdcc3869c4790b
Ryan <ryanchien8125@gmail.com>
2022-09-22 22:19:31 +0800

    使用 Nuxt Tailwind 模組導入 Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './composables/**/*.{js,ts}',
+    './plugins/**/*.{js,ts}',
+    './app.{js,ts,vue}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
