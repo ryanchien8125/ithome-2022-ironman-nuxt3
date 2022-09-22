6eec4a7c3feca84496e14b9e17671cbe29f85190
Ryan <ryanchien8125@gmail.com>
2022-09-22 22:21:48 +0800

    建立一個自動導入的元件

---
+++ components/IronManWelcome.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="bg-white py-24">
+    <div class="flex flex-col items-center">
+      <h1 class="text-6xl font-semibold text-sky-400">2022 iThome</h1>
+      <p class="mt-4 text-9xl font-bold text-gray-600">鐵人賽</p>
+    </div>
+  </div>
+</template>
