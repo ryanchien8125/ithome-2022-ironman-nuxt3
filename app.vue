2e9c4712316072a1e68b9e3eace67ec08958bad2
Ryan <ryanchien8125@gmail.com>
2022-09-23 20:10:31 +0800

    建立一個預設的布局模板

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="m-4 bg-white">
+    <p class="pb-4 text-2xl text-slate-600">這裡是最外層 app.vue</p>
+    <NuxtLayout name="default"></NuxtLayout>
   </div>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="bg-sky-100 py-2">
+    <p class="px-6 py-4 text-2xl text-gray-700">這是預設的布局，全部頁面都會使用到</p>
+    <slot />
+  </div>
+</template>
