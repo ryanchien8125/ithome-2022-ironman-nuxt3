4fbc36e2d2b5dfd8ed6daa660b09144164b154ee
Ryan <ryanchien8125@gmail.com>
2022-09-23 22:48:28 +0800

    更進階的指定布局模板

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -4,5 +4,6 @@
       使用 <span class="font-bold text-rose-500">Custom</span> 布局
     </p>
     <slot />
+    <slot name="footer" />
   </div>
 </template>
--- pages/custom.vue
+++ pages/custom.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class="mx-6 mb-4 bg-white py-24">
-    <div class="flex flex-col items-center">
-      <h1 class="text-6xl font-semibold text-sky-400">2022 iThome</h1>
-      <p class="mt-4 text-9xl font-bold text-gray-600">鐵人賽</p>
-    </div>
-  </div>
+  <NuxtLayout name="custom">
+    <template #default>
+      <div class="mx-6 mb-4 bg-white py-24">
+        <div class="flex flex-col items-center">
+          <h1 class="text-6xl font-semibold text-sky-400">2022 iThome</h1>
+          <p class="mt-4 text-9xl font-bold text-gray-600">鐵人賽</p>
+        </div>
+      </div>
+    </template>
+    <template #footer>
+      <div class="flex flex-col items-center">
+        <p class="mt-4 text-xl text-slate-600">感謝您閱讀 Nuxe 3 學習筆記～</p>
+      </div>
+    </template>
+  </NuxtLayout>
 </template>
 
 <script setup>
 definePageMeta({
-  layout: 'custom'
+  layout: false
 })
 </script>
