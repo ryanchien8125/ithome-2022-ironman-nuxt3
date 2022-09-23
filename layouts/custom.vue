872f8d1e89b5f7dedce81baff7d875dadb78d7ab
Ryan <ryanchien8125@gmail.com>
2022-09-23 22:36:51 +0800

    使用指定的布局模板

---
+++ layouts/custom.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="bg-rose-100 py-2">
+    <p class="px-6 py-4 text-2xl text-gray-700">
+      使用 <span class="font-bold text-rose-500">Custom</span> 布局
+    </p>
+    <slot />
+  </div>
+</template>
