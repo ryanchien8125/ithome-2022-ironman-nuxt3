39a0bbe98f9804bd791a581b6dcdeb518c07b03c
Ryan <ryanchien8125@gmail.com>
2022-09-23 21:48:56 +0800

    在布局模板中建立多個插槽

--- app.vue
+++ app.vue
@@ -2,9 +2,17 @@
   <div class="m-4 bg-white">
     <p class="pb-4 text-2xl text-slate-600">這裡是最外層 app.vue</p>
     <NuxtLayout name="default">
-      <p class="px-6 pt-4 text-xl text-slate-800">
-        被 NuxtLayout 包裹的元件將會放置到 Layout 的 solt 中
-      </p>
+      <template #header>
+        <p class="px-6 pt-4 text-xl text-green-500">這段會放置在 header 插槽</p>
+      </template>
+      <template #default>
+        <p class="px-6 pt-4 text-xl text-cyan-500">
+          被 NuxtLayout 包裹的元件將會放置到 Layout 的預設 solt 中
+        </p>
+      </template>
+      <template #footer>
+        <p class="px-6 pt-4 text-xl text-blue-500">這段會放置在 footer 插槽</p>
+      </template>
     </NuxtLayout>
   </div>
 </template>
