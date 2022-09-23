128c3a5bd7a1670b0bcb5076cd5acd31beb30f4d
Ryan <ryanchien8125@gmail.com>
2022-09-23 21:50:50 +0800

    建立布局模板與路由頁面

--- app.vue
+++ app.vue
@@ -1,18 +1,8 @@
 <template>
   <div class="m-4 bg-white">
     <p class="pb-4 text-2xl text-slate-600">這裡是最外層 app.vue</p>
-    <NuxtLayout name="default">
-      <template #header>
-        <p class="px-6 pt-4 text-xl text-green-500">這段會放置在 header 插槽</p>
-      </template>
-      <template #default>
-        <p class="px-6 pt-4 text-xl text-cyan-500">
-          被 NuxtLayout 包裹的元件將會放置到 Layout 的預設 solt 中
-        </p>
-      </template>
-      <template #footer>
-        <p class="px-6 pt-4 text-xl text-blue-500">這段會放置在 footer 插槽</p>
-      </template>
+    <NuxtLayout>
+      <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="m-6 bg-slate-50 py-24">
+    <div class="flex flex-col items-center">
+      <h1 class="text-6xl font-semibold text-sky-400">2022 iThome</h1>
+      <p class="mt-4 text-9xl font-bold text-gray-600">鐵人賽</p>
+    </div>
+  </div>
+</template>
