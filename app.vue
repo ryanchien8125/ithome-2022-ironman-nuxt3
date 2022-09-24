02883dc2e7fcbb87f151b1007af00cc977b57b13
Ryan <ryanchien8125@gmail.com>
2022-09-24 17:43:40 +0800

    建立延遲載入按鈕

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="flex flex-col items-center">
+    <div class="mt-8 flex items-center">
+      <input id="show-button" v-model="show" name="show-button" type="checkbox" class="h-5 w-5" />
+      <label for="show-button" class="ml-2 block text-base text-slate-800">顯示報名按鈕</label>
+    </div>
+    <LazyBaseApplyButton v-if="show" />
   </div>
 </template>
+
+<script setup>
+const show = ref(false)
+</script>
