efdec1dfac681d74a4930dee2c3bd657167ac93b
Ryan <ryanchien8125@gmail.com>
2022-09-26 22:18:21 +0800

    自定義插件

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="bg-white py-24">
+    <div class="flex flex-col items-center">
+      <h1 class="my-2 text-6xl font-semibold text-sky-400">{{ $hello('Ryan') }}</h1>
+      <h1 class="my-2 text-6xl font-semibold text-emerald-400">{{ title }}</h1>
+    </div>
   </div>
 </template>
+
+<script setup>
+const { $hello } = useNuxtApp()
+const title = $hello('Jennifer')
+</script>
