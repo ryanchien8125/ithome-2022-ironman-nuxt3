02af232f4adead39e881fd05e1da60a11826a178
Ryan <ryanchien8125@gmail.com>
2022-09-26 23:17:23 +0800

    使用 vue-gtag 插件

---
+++ plugins/vue-gtag.client.js
@@ -0,0 +1,9 @@
+import VueGtag from 'vue-gtag-next'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueGtag, {
+    property: {
+      id: 'GA_MEASUREMENT_ID'
+    }
+  })
+})
