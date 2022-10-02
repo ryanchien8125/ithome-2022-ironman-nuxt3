4a565d05516e25e717b362ee1e4332ceb6e681dc
Ryan <ryanchien8125@gmail.com>
2022-10-02 23:33:09 +0800

    安裝 Pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
   typescript: {
     typeCheck: true
   }
