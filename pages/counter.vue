d77e12df0d7001c5d05740472eae854c691ee52f
Ryan <ryanchien8125@gmail.com>
2022-10-02 23:37:11 +0800

    調整 counterStore

--- pages/counter.vue
+++ pages/counter.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="bg-white py-24">
     <div class="flex flex-col items-center">
-      <span class="text-9xl font-semibold text-sky-600">{{ counterStore.count }}</span>
+      <span class="text-9xl font-semibold text-sky-600">{{ count }}</span>
       <div class="mt-8 flex flex-row">
         <button
           class="font-base mx-2 rounded-full bg-sky-500 px-4 py-2 text-xl text-white hover:bg-sky-600 focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2"
-          @click="counterStore.increment"
+          @click="increment"
         >
           增加
         </button>
         <button
           class="font-base mx-2 rounded-full bg-sky-500 px-4 py-2 text-xl text-white hover:bg-sky-600 focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2"
-          @click="counterStore.decrement"
+          @click="decrement"
         >
           減少
+        </button>
+      </div>
+      <div class="mt-8 flex justify-center">
+        <button
+          class="font-base mx-2 rounded-full bg-orange-500 px-4 py-2 text-xl text-white hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-400 focus:ring-offset-2"
+          @click="counterStore.$reset"
+        >
+          重置
         </button>
       </div>
       <div class="mt-8">
@@ -24,7 +32,11 @@
 </template>
 
 <script setup>
+import { storeToRefs } from 'pinia'
 import { useCounterStore } from '@/stores/counter'
 
 const counterStore = useCounterStore()
+
+const { count } = storeToRefs(counterStore)
+const { increment, decrement } = counterStore
 </script>
