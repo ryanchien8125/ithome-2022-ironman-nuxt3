4292967fc3989624d7e865cccfa38bf5faac7446
Ryan <ryanchien8125@gmail.com>
2022-10-04 22:00:28 +0800

    使用 googleTokenLogin()

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="bg-white py-24">
+    <div class="flex flex-col items-center">
+      <h1 class="text-6xl font-semibold text-gray-800">這裡是首頁</h1>
+      <div class="my-4 flex space-x-4">
+        <NuxtLink to="/login">前往 Login</NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
