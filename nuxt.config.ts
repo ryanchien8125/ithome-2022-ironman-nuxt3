53fbfe00c22012b2272489311c8334ea2976463f
Ryan <ryanchien8125@gmail.com>
2022-10-07 23:47:00 +0800

    安裝 @headlessui/vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  build: {
+    transpile: ['@headlessui/vue']
+  },
   modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@pinia/nuxt'],
   typescript: {
     typeCheck: true
