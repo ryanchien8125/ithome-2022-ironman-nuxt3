b57c819dafc0c2274ead61a3b64b702ae94f49a2
Ryan <ryanchien8125@gmail.com>
2022-10-07 23:42:45 +0800

    添加一個預設布局模板

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <header class="flex w-full justify-center">
+      <nav class="flex w-full max-w-7xl items-center justify-between py-2">
+        <div>
+          <a aria-label="TailwindBlog" href="/">
+            <div class="flex items-center justify-between">
+              <div class="mr-3">
+                <Icon class="h-12 w-12" name="logos:nuxt-icon" />
+              </div>
+              <div class="hidden h-6 text-2xl font-semibold text-gray-700 sm:block">
+                Nuxt 3 Blog
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="flex items-center text-base leading-5">
+          <div class="block">
+            <NuxtLink class="px-3 py-2 text-gray-700 transition hover:text-emerald-500" to="/login">
+              登入
+            </NuxtLink>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <slot />
+  </div>
+</template>
