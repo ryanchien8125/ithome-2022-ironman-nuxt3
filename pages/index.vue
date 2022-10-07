c6a0a692e084531c26a5fea314a314cee3046033
Ryan <ryanchien8125@gmail.com>
2022-10-07 20:03:43 +0800

    移除用不到的程式碼

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,6 @@
       <h1 class="text-6xl font-semibold text-gray-800">這裡是首頁</h1>
       <div class="my-4 flex space-x-4">
         <NuxtLink to="/login">前往 Login</NuxtLink>
-        <NuxtLink to="/whoami">前往 Whoami</NuxtLink>
       </div>
     </div>
   </div>
