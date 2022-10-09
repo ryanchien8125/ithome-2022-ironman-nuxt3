2f9d56c14d06e265d4065b3d185dde0336ec740f
Ryan <ryanchien8125@gmail.com>
2022-10-09 23:54:41 +0800

    實戰添加 Meta

--- pages/articles/[id].vue
+++ pages/articles/[id].vue
@@ -23,4 +23,16 @@ const route = useRoute()
 const { data: article } = await useFetch(`/api/articles/${route.params.id}`, {
   initialCache: false
 })
+
+useHead({
+  meta: [
+    { name: 'description', content: `${article.value.summary} | Nuxt 3 Blog` },
+    { name: 'keywords', content: `${article.value.tags}` },
+    { property: 'og:title', content: `${article.value.title} | Nuxt 3 Blog` },
+    { property: 'og:description', content: article.value.summary },
+    { property: 'og:image', content: article.value.cover },
+    { property: 'og:image', content: article.value.cover }
+  ],
+  title: `${article.value.title} | Nuxt 3 Blog`
+})
 </script>
