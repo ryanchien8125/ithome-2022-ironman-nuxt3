d3480ec671cb797add34afcc1fb83d0506ddf833
Ryan <ryanchien8125@gmail.com>
2022-10-09 21:07:47 +0800

    添加動態網頁的標題

--- app.vue
+++ app.vue
@@ -7,3 +7,13 @@
     <NotificationContainer />
   </div>
 </template>
+
+<script setup>
+const title = ref('Nuxt 3 Blog')
+useHead({
+  title,
+  titleTemplate: (title) => {
+    return title ? `${title} - 首頁` : '首頁'
+  }
+})
+</script>
--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -78,4 +78,10 @@ watch(error, (newError) => {
     pushNotify('error', '取得文章失敗', error.value?.data?.message ?? '未知錯誤')
   }
 })
+
+useHead({
+  titleTemplate: (title) => {
+    return title ? `${title} - 所有文章` : '所有文章'
+  }
+})
 </script>
