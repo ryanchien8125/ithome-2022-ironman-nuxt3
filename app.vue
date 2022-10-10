055b48aa0e46902f26c7de4e03dbf660088f8df0
Ryan <ryanchien8125@gmail.com>
2022-10-10 22:34:35 +0800

    使用 Local Storage 持久化語系設定

--- app.vue
+++ app.vue
@@ -3,3 +3,14 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup>
+import { useI18n } from 'vue-i18n'
+const { locale, setLocale } = useI18n()
+
+const defaultLocaleCode = locale.value
+
+if (process.client) {
+  setLocale(localStorage.getItem('i18n-lang') ?? defaultLocaleCode)
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -5,19 +5,13 @@
     </h1>
     <div class="mt-24 space-x-4">
       <button
+        v-for="localeItem in locales"
+        :key="localeItem.code"
         type="button"
         class="inline-flex items-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-700 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-        @click="setLocale('en')"
+        @click="changeLanguage(localeItem.code)"
       >
-        English
-      </button>
-
-      <button
-        type="button"
-        class="inline-flex items-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-700 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-        @click="setLocale('zh')"
-      >
-        繁體中文
+        {{ localeItem.name }}
       </button>
     </div>
     <div class="mt-4 flex flex-row justify-center">
@@ -29,5 +23,10 @@
 
 <script setup>
 import { useI18n } from 'vue-i18n'
-const { locale, setLocale } = useI18n()
+const { locale, locales, setLocale } = useI18n()
+
+const changeLanguage = (localeCode) => {
+  setLocale(localeCode)
+  localStorage.setItem('i18n-lang', localeCode)
+}
 </script>
