043987bd26178073fe72e7e3933a7b46f6a04d0a
Ryan <ryanchien8125@gmail.com>
2022-11-05 01:48:54 +0800

    Upgrade to Nuxt v3.0.0-rc.13

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  build: {
-    postcss: {
-      postcssOptions: {
-        plugins: {
-          'postcss-import': {},
-          'tailwindcss/nesting': {},
-          tailwindcss: {},
-          autoprefixer: {}
-        }
-      }
+  postcss: {
+    plugins: {
+      'postcss-import': {},
+      'tailwindcss/nesting': {},
+      tailwindcss: {},
+      autoprefixer: {}
     }
   },
   css: ['@/assets/css/tailwind.css'],
