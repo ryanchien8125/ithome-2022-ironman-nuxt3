011f1899733561a40d7017a3d557ec14d5a596c3
Ryan <ryanchien8125@gmail.com>
2023-10-16 00:45:42 +0800

    升級套件版本與設定

--- components/base/apply/BaseApplyButton.vue
+++ components/base/apply/BaseApplyButton.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     type="submit"
-    class="mt-6 rounded-sm bg-blue-600 py-3 px-8 text-xl font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+    class="mt-6 rounded-sm bg-blue-600 px-8 py-3 text-xl font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
   >
     立即報名
   </button>
