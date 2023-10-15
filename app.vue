18ebbd535e69902523064475a0a9d7d4d09aee5d
Ryan <ryanchien8125@gmail.com>
2023-10-16 00:53:41 +0800

    升級套件版本與設定

--- app.vue
+++ app.vue
@@ -4,13 +4,13 @@
       <h1 class="my-2 text-6xl font-semibold text-sky-400">{{ $hello('Ryan') }}</h1>
       <h1 class="my-2 text-6xl font-semibold text-emerald-400">{{ title }}</h1>
       <button
-        class="mt-6 rounded-sm bg-blue-500 py-3 px-8 text-xl font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
+        class="mt-6 rounded-sm bg-blue-500 px-8 py-3 text-xl font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
       >
         這個按鈕沒有 v-focus
       </button>
       <button
         v-focus
-        class="mt-6 rounded-sm bg-blue-500 py-3 px-8 text-xl font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
+        class="mt-6 rounded-sm bg-blue-500 px-8 py-3 text-xl font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
       >
         這個按鈕會自動成為焦點
       </button>
--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,4 +1,4 @@
-import VueGtag from 'vue-gtag-next'
+import VueGtag from 'vue-gtag'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
