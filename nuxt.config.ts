d9785948100e3a3c2bda0bddebb4ca318ed661b9
Ryan <ryanchien8125@gmail.com>
2023-10-16 04:25:12 +0800

    升級套件版本與設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,25 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
   typescript: {
     typeCheck: true
   },
   i18n: {
-    defaultLocale: 'zh',
     langDir: 'locales',
     locales: [
-      { code: 'en', file: 'en.json', iso: 'en-US', name: 'English' },
-      { code: 'zh', file: 'zh.json', iso: 'zh-TW', name: '繁體中文' }
+      {
+        code: 'en',
+        iso: 'en-US',
+        file: 'en.json'
+      },
+      {
+        code: 'zh',
+        iso: 'zh-TW',
+        file: 'zh.json'
+      }
     ],
-    strategy: 'no_prefix',
-    vueI18n: {
-      legacy: false
-    }
-  }
+    defaultLocale: 'zh',
+    strategy: 'no_prefix'
+  },
+  devtools: { enabled: true }
 })
