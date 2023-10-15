fa5240dbf224aa82b6b8a517a5fe1c0d42d2d026
Ryan <ryanchien8125@gmail.com>
2023-10-16 00:34:36 +0800

    升級套件版本與設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   typescript: {
     typeCheck: true
-  }
+  },
+  devtools: { enabled: true }
 })
