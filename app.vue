29f61d0cc9829fbe2100de16322c83ff16047cf1
Ryan <ryanchien8125@gmail.com>
2023-10-16 00:32:18 +0800

    升級套件版本與設定

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <IronManWelcome />
+  <div class="bg-white py-24">
+    <div class="flex flex-col items-center">
+      <h1 class="text-6xl font-semibold text-blue-600">2022 iThome</h1>
+      <p class="mt-4 text-9xl font-bold text-gray-900">鐵人賽</p>
+    </div>
   </div>
 </template>
